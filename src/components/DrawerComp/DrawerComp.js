5cf6dd556195028655fdc454fa521c52654d9286
Mohini Mohan Raut <mohiniraut7585@gmail.com>
2023-02-01 22:22:20 +0530

    drawer change

--- src/components/DrawerComp/DrawerComp.js
+++ src/components/DrawerComp/DrawerComp.js
@@ -1,36 +1,77 @@
-import React, { useState } from 'react'
-import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+// import React, { useState } from 'react'
+// import { Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
+// import MenuIcon from '@mui/icons-material/Menu';
 
-const PAGES = ["Home", "Task", "User", "Login", "Logout"]
+// const PAGES = ["HomePage", "Task", "User","Logout"]
+
+// function DrawerComp() {
+//     const [openDrawer, setOpenDrawer] = useState(false)
+//     return (
+//         <>
+//             <Drawer open={openDrawer}
+//                 onClose={() => setOpenDrawer(false)}
+//             >
+//                 <List>
+//                     {
+//                         PAGES.map((page, index) => (
+//                             <ListItemButton onClick={() => setOpenDrawer(false)} key={index}>
+//                                 <ListItemIcon>
+//                                     <ListItemText>{page}</ListItemText>
+
+
+//                                 </ListItemIcon>
+//                             </ListItemButton>
+//                         ))
+//                     }
+
+//                 </List>
+//             </Drawer>
+//             <IconButton sx={{ color: "white", marginLeft: "auto" }} onClick={() => setOpenDrawer(!openDrawer)}>
+//                 <MenuIcon />
+//             </IconButton>
+//         </>
+//     )
+// }
+
+// export default DrawerComp
+
+
+import React, { useState } from 'react'
+import {Drawer,List, ListItemButton, ListItemIcon, ListItemText,IconButton,useMediaQuery,useTheme} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
+
+const PAGES=["HomePage", "Task", "User","Logout"]
 
 function DrawerComp() {
-    const [openDrawer, setOpenDrawer] = useState(false)
-    return (
-        <>
-            <Drawer open={openDrawer}
-                onClose={() => setOpenDrawer(false)}
-            >
-                <List>
-                    {
-                        PAGES.map((page, index) => (
-                            <ListItemButton onClick={() => setOpenDrawer(false)} key={index}>
-                                <ListItemIcon>
-                                    <ListItemText>{page}</ListItemText>
-
-
-                                </ListItemIcon>
-                            </ListItemButton>
-                        ))
-                    }
-
-                </List>
-            </Drawer>
-            <IconButton sx={{ color: "white", marginLeft: "auto" }} onClick={() => setOpenDrawer(!openDrawer)}>
-                <MenuIcon />
-            </IconButton>
-        </>
-    )
+  const [openDrawer,setOpenDrawer]=useState(false)
+const theme=useTheme();
+console.log(theme)
+const isMatch=useMediaQuery(theme.breakpoints.down('md'));
+console.log(isMatch)
+  return (
+    <React.Fragment>
+        <Drawer open={openDrawer}
+        onClose={()=>setOpenDrawer(false)}
+        >
+          <List>
+            {
+              PAGES.map((page,index)=>(
+       <ListItemButton onClick={()=>setOpenDrawer(false)} key={index} component={Link} to={`/${page}`}>
+              <ListItemIcon >
+                <ListItemText >{page}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+              ))
+            }
+            
+          </List>
+        </Drawer>
+        <IconButton sx={{color:'white',marginLeft:'auto'}} onClick={()=>setOpenDrawer(!openDrawer)}>
+          <MenuIcon/>
+        </IconButton>
+    </React.Fragment>
+  )
 }
 
-export default DrawerComp
+export default DrawerComp;
